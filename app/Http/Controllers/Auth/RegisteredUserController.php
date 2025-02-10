8aad458e9b3292bd1d7ed49c99105c535ca4ef00
IvnCr <icr0015@alu.medac.es>
2025-02-10 18:07:44 +0100

    Arreglo?

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -34,12 +34,15 @@ class RegisteredUserController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|lowercase|email|max:255|unique:'.User::class,
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'idSede' => ['required|exists:sedes,id'],
+
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'idSede' => $request->idSede,
         ]);
 
         event(new Registered($user));
