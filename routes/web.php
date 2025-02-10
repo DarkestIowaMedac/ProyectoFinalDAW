450d728f35fadef63048f40ad283739129c0faf7
IvnCr <icr0015@alu.medac.es>
2025-02-10 18:17:40 +0100

    web.php

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
-use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Application;
+use App\Http\Controllers\SedeController;
+use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -23,5 +24,12 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+Route::middleware('auth')->group(function () {
+    Route::post('/muestras', [MuestraController::class, 'crearMuestra']); 
+    Route::get('/muestras/{idMuestra}', [MuestraController::class, 'verMuestra']); 
+    Route::delete('/muestras/{idMuestra}', [MuestraController::class, 'borrarMuestra']); 
+});
+
+Route::get('/sedes', [SedeController::class, 'index']);
 
 require __DIR__.'/auth.php';
