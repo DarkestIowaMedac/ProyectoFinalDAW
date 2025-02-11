0fda5e3f0aa4bc1cfe9fea0a647c2eeaef42d8b2
javi19xx <fsc0005@alu.medac.es>
2025-02-11 17:59:39 +0100

    Validación correcta idSede
    He creado una nueva validación para idSede de forma que si no encuentra salte un mensaje de error(Creado en RegisteredUserController)

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -2,16 +2,17 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
+use App\Models\Sede;
 use App\Models\User;
-use Illuminate\Auth\Events\Registered;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\Rules;
 use Inertia\Inertia;
 use Inertia\Response;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rules;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Auth\Events\Registered;
 
 class RegisteredUserController extends Controller
 {
@@ -34,8 +35,11 @@ class RegisteredUserController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|lowercase|email|max:255|unique:' . User::class,
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
-            'idSede' => ['required'],
-//exists:sede,id'
+            'idSede' => ['required', function ($attribute, $value, $fail) {
+                if(!Sede::find($value)){
+                    $fail('Sede no encontrada.');
+                }
+            }],
         ]);
 
         $user = User::create([
