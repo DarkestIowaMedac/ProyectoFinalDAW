c58c642466eac923ece629ecbd3529138581e3e1
DarkestIowaMedac <153643708+DarkestIowaMedac@users.noreply.github.com>
2025-02-11 16:19:40 +0100

    error del protocolo de violación

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Vite;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -12,9 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->bind(ImageManager::class, function() {
-            return new ImageManager(['driver' => 'gd']); // o 'imagick'
-        });
+        // $this->app->bind(ImageManager::class, function() {
+        //     return new ImageManager(['driver' => 'gd']); // o 'imagick'
+        // });
     }
 
     /**
@@ -22,6 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Schema::defaultStringLength(191);
         Vite::prefetch(concurrency: 3);
     }
 }
