c8409f2e1471305d3748931c0bfeed1636786f44
JesusGomez99 <jgl0033@alu.medac.es>
2025-02-12 17:47:26 +0100

    cambio en el layout de la muestra

--- resources/js/Layouts/FormInsertLayout.jsx
+++ resources/js/Layouts/FormInsertLayout.jsx
@@ -19,7 +19,7 @@ export default function FormInsertLayout() {
                     <PersInputBiopsia />
                     <PersInputCalidad />
                     <PersInputInterpretacion />
-                    <PersInputFotos></PersInputFotos>
+                    <PersInputFotos/>
                 </div>
             </form>
         </div>
